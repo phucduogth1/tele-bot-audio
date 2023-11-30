993bd937a2d4210a9f6847b429743c7818c04d8a
Phuc Duong <phucduogth1@gmail.com>
2023-12-01 02:21:52 +0700

    upgrade to recommend music

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import vlc
 from telegram import Update
 from telegram.ext import ApplicationBuilder, filters, CommandHandler, MessageHandler, ContextTypes
 from dotenv import load_dotenv
+from ytmusicapi import YTMusic
 
 # Initialize services
 load_dotenv()
+ytmusic = YTMusic("oauth.json")
 media_player = vlc.MediaListPlayer()
 vlc_instance = vlc.Instance()
 media_list = vlc_instance.media_list_new()
@@ -16,19 +18,27 @@ current_last_song_index = 0
 # Initialize vars
 TELE_BOT_TOKEN = os.getenv('TELE_BOT_TOKEN')
 playlist_info = []
+recommend_playlist = []
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-def get_audio_stream_url(video_url):
+def get_audio_stream_by_url(video_url):
     yt = YouTube(video_url)
     yt.check_availability()
     video_title = yt.title
     video_author = yt.author
     audio_stream = yt.streams.filter(only_audio=True).first()
-    print(audio_stream.url)
+    return video_title, video_author, audio_stream.url, yt.video_id
+
+def get_audio_stream_by_video_id(video_id):
+    yt = YouTube.from_id(video_id)
+    yt.check_availability()
+    video_title = yt.title
+    video_author = yt.author
+    audio_stream = yt.streams.filter(only_audio=True).first()
     return video_title, video_author, audio_stream.url
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -40,11 +50,34 @@ async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
+async def watch(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    video_url = update.message.text
+    try:
+        video_title, video_author, audio_stream_url, video_id = get_audio_stream_by_url(video_url=video_url)
+        # songs = ytmusic.get_song(videoId=video_id)
+        # watch_pl = ytmusic.get_watch_playlist(video_id)
+        # playlistId = ytmusic.create_playlist("test", "test description")
+        # print(f'ID:> {video_id} | {playlistId}')
+        # search_results = ytmusic.search("Oasis Wonderwall")
+        # news = ytmusic.add_playlist_items(playlistId, [video_id])
+        # songs = ytmusic.get_playlist(playlistId)
+        # print(f'NEWS: {news}')
+        # formatted_json_songs = json.loads(songs)
+        watch_pl = ytmusic.get_watch_playlist(video_id)
+        print(f'WatchPL: {watch_pl}')
+        # print(f'SONGS: {songs}')
+        # formatted_json = json.loads(watch_pl)
+        print(f'FORMATTED:')
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"songs")
+    except Exception as e:
+        print(f"Error processing URL: {e}")
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Invalid URL. Provide a valid audio stream URL.")
+
 # Function to add audio stream URL to the queue
 async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
     video_url = update.message.text
     try:
-        video_title, video_author, audio_stream_url = get_audio_stream_url(video_url=video_url)
+        video_title, video_author, audio_stream_url, video_id = get_audio_stream_by_url(video_url=video_url)
 
         if len(audio_stream_url) > 1:
             media = vlc_instance.media_new(audio_stream_url)
@@ -54,12 +87,11 @@ async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
             
             # setting media list to the media player
             media_player.set_media_list(media_list)
+            
             playlist_info.append(
-                {
-                    "video_title": video_title,
-                    "video_author": video_author
-                }
+                {'title': video_title, 'artist': video_author, 'video_id': video_id, 'stream_audio': audio_stream_url}
             )
+            get_recommend_playlist() 
             await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Added *{video_title}* to queue.", parse_mode="Markdown")
         else:
             await context.bot.send_message(chat_id=update.effective_chat.id, text="Invalid URL. Provide a valid audio stream URL.")
@@ -69,22 +101,25 @@ async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Function to play the current audio stream
 async def play(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global current_last_song_index
+
     specific_song_index = update.message.text
     if len(specific_song_index.split()) == 2:
         if specific_song_index.split()[1].isnumeric():
             index_query = int(specific_song_index.split()[1])
             if index_query >= 0 and index_query <= len(playlist_info):
                 media_player.play_item_at_index(index_query)
+                current_last_song_index = index_query
                 current_song = get_current_track()
                 await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Now playing: *{current_song}*", parse_mode="Markdown")
             else:
                 await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Media index out of range between {0} - {len(playlist_info)}")
         else:
             await context.bot.send_message(chat_id=update.effective_chat.id, text="Wrong argument type number.")
-    elif current_last_song_index < len(playlist_info) and current_last_song_index != 0 and not media_player.is_playing():
-        media_player.play_item_at_index(current_last_song_index + 1)
-        current_song = get_current_track()
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Now playing: *{current_song}*", parse_mode="Markdown")
+    # elif current_last_song_index < len(playlist_info) and current_last_song_index != 0 and not media_player.is_playing():
+    #     media_player.play_item_at_index(current_last_song_index + 1)
+    #     current_song = get_current_track()
+    #     await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Now playing: *{current_song}*", parse_mode="Markdown")
     elif current_last_song_index == 0:
         media_player.play()
         current_song = get_current_track()
@@ -107,16 +142,27 @@ async def resume(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Function to skip to the next track
 async def next_track(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global current_last_song_index
+
     canNext = media_player.next()
+    current_last_song_index += 1
     if canNext == 0:
+        print(f'---------NEXT_IN_PLAYLIST-------')
         current_track = get_current_track()
         await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Playing *{current_track}*", parse_mode="Markdown")
     else:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="No more tracks in queue.")
+        print(f'---------NEXT_IN_RECOMMEND_LIST-------')
+        play_recommend_track()
+        media_player.play_item_at_index(current_last_song_index)
+        current_track = get_current_track()
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Playing *{current_track}*", parse_mode="Markdown")
 
 # Function to play the previous track
 async def prev_track(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global current_last_song_index
+
     canBack = media_player.previous()
+    current_last_song_index -= 1
     if canBack == 0:
         current_track = get_current_track()
         await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Playing *{current_track}*", parse_mode="Markdown")
@@ -142,7 +188,7 @@ async def playlist(update: Update, context: ContextTypes.DEFAULT_TYPE):
     current_song_index = get_current_track_index()
     
     for idx, song_info in enumerate(playlist_info, start=0):
-        song_text = f"{idx}. {song_info['video_title']}. Channel: {song_info['video_author']}"
+        song_text = f"{idx}. {song_info['title']}. Artists: {song_info['artist']}"
         if current_song_index and idx == current_song_index:
             song_text += "  *<<<<Now playing*"
         playlist_text += song_text + "\n"
@@ -172,7 +218,7 @@ def set_volume(volume):
 def get_current_track():
     if not len(playlist_info) == 0:
         index = get_current_track_index()
-        return playlist_info[index]["video_title"]
+        return playlist_info[index]["title"]
 
 def get_current_track_index():
     if not len(playlist_info) == 0:
@@ -182,8 +228,40 @@ def get_current_track_index():
 # def assign_last_index():
 def on_end_reached(event):
     global current_last_song_index
+    print(f'---------CURRENT_LAST_SONG_INDEX: {current_last_song_index} | PLAYLIST_INFO_LENGTH: {len(playlist_info)}---------')
+    if len(recommend_playlist) == 0:
+        print('---------REACH_END_RECOMMEND_PLAYLIST---------')
+        get_recommend_playlist()
+    if current_last_song_index == (len(playlist_info)-1):
+        print('---------REACH_END_PLAYLIST---------')
+        play_recommend_track()
+        media_player.play()
     current_last_song_index += 1
+
+
+def play_recommend_track():
+    global current_last_song_index
+
+    recommend_track = recommend_playlist.pop(0)
+    _, _, audio_stream = get_audio_stream_by_video_id(recommend_track['video_id'])
+    print(f'---------RECOMMEND_TRACK: {recommend_track}---------')
+    playlist_info.append(recommend_track)
+    media = vlc_instance.media_new(audio_stream)
+    media_list.add_media(media)
+    media_player.set_media_list(media_list)
     
+    
+
+def get_recommend_playlist():
+    recommend_playlist.clear()
+    watch_pl = ytmusic.get_watch_playlist(playlist_info[-1]['video_id'], limit=1)
+    print(f'---------WATCH_OF: {playlist_info[-1]["title"]}---------')
+    for track in watch_pl['tracks']:
+        # _, _, audio_stream = get_audio_stream_by_video_id(track['videoId'])
+        track_data = {'title': track['title'], 'artist': track['artists'][0]['name'], 'video_id': track['videoId'], 'stream_audio': None}
+        recommend_playlist.append(track_data)
+    recommend_playlist.pop(0)
+    print(f'---------RECOMMEND_NEXT_TRACK: {recommend_playlist[0]}---------')
 
 # Initialize the Telegram bot
 def main() -> None:
@@ -192,6 +270,7 @@ def main() -> None:
     event_manager.event_attach(vlc.EventType.MediaPlayerEndReached, on_end_reached)
     # Command handlers
     start_handler = CommandHandler("start", start)
+    watch_handler = CommandHandler("watch", watch)
     add_handler = CommandHandler("add", add)
     play_handler = CommandHandler("play", play)
     pause_handler = CommandHandler("pause", pause)
@@ -207,6 +286,7 @@ def main() -> None:
     
     application.add_handler(echo_handler)
     application.add_handler(start_handler)
+    application.add_handler(watch_handler)
     application.add_handler(add_handler)
     application.add_handler(play_handler)
     application.add_handler(pause_handler)
